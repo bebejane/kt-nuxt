0fe414d63e7102919b2463e61c6cdd6fd7a1148d
Bebe Jane <bjornthief@gmail.com>
2025-10-14 20:47:54 +0200

    update

--- app/components/Markdown.vue
+++ app/components/Markdown.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import VueMarkdown from 'vue-markdown-render';
+import { type Options as MarkdownItOptions, type PluginSimple } from 'markdown-it';
 
-type Props = Partial<Omit<InstanceType<typeof VueMarkdown>, `$${string}`>>;
+type Props = {
+	source?: string;
+	options?: MarkdownItOptions;
+	plugins?: PluginSimple[];
+};
+
 const props = defineProps<Props>();
 </script>
 
