b87f4376ebe5122868206e3d79e7b38f6ffb7b0b
Bebe Jane <bjornthief@gmail.com>
2025-10-14 15:15:06 +0200

    mer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 import { defineNuxtConfig } from 'nuxt/config';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const appDir = dirname(fileURLToPath(import.meta.url))
+	.split('/')
+	.slice(0, -5)
+	.join('/');
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -7,4 +14,36 @@ export default defineNuxtConfig({
 	},
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@use "${appDir}/assets/styles/_mediaqueries.scss" as *;`,
+				},
+			},
+		},
+	},
+	$production: {
+		routeRules: {
+			'/**': { isr: true },
+		},
+	},
+	experimental: {
+		typedPages: true,
+	},
+	runtimeConfig: {
+		private: {
+			apiToken: process.env.DATOCMS_API_TOKEN,
+		},
+		public: {
+			apiToken: process.env.PUBLIC_DATOCMS_API_TOKEN,
+		},
+	},
+	nitro: {
+		vercel: {
+			config: {
+				bypassToken: process.env.REVALIDATE_KEY,
+			},
+		},
+	},
 });
