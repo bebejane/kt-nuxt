b9925fa4b573646de30c084240c1d17294326303
Bebe Jane <bjornthief@gmail.com>
2025-10-14 15:18:52 +0200

    hej

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
 		css: {
 			preprocessorOptions: {
 				scss: {
-					additionalData: `@use "${rootDir}/app/assets/styles/_mediaqueries.scss" as *;`,
+					additionalData: `@use "~/app/assets/styles/_mediaqueries.scss" as *;`,
 				},
 			},
 		},
