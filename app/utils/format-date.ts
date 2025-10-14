6beba934843eab16aaf03d67bc739d8401188b04
Bebe Jane <bjornthief@gmail.com>
2025-10-14 15:00:47 +0200

    psps

--- app/utils/format-date.ts
+++ app/utils/format-date.ts
@@ -1,9 +1,9 @@
-export default function formatDate(date: string) {
+export default function formatDate(date: string, locale = 'en-US') {
 	const dateObj = new Date(date);
 	const options: Intl.DateTimeFormatOptions = {
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric',
 	};
-	return dateObj.toLocaleDateString('en-US', options);
+	return dateObj.toLocaleDateString(locale, options);
 }
