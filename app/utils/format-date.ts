61e75950f4bd4326a68b8f419d5525a25eb57fe6
Bebe Jane <bjornthief@gmail.com>
2025-10-14 15:00:02 +0200

    formatdate

---
+++ app/utils/format-date.ts
@@ -0,0 +1,9 @@
+export default function formatDate(date: string) {
+	const dateObj = new Date(date);
+	const options: Intl.DateTimeFormatOptions = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	};
+	return dateObj.toLocaleDateString('en-US', options);
+}
