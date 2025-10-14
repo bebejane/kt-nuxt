97c14683fef6315afda46c4a3e40f99ae8b1c2d6
Bebe Jane <bjornthief@gmail.com>
2025-10-14 20:42:06 +0200

    update

--- app/components/Markdown.vue
+++ app/components/Markdown.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import VueMarkdown from 'vue-markdown-render';
 
-type Props = InstanceType<typeof VueMarkdown>['$props'];
-
+type Props = Partial<Omit<InstanceType<typeof VueMarkdown>, `$${string}`>>;
 const props = defineProps<Props>();
 </script>
+
 <template :if="props.source !== undefined">
 	<VueMarkdown :source="props.source as string" />
 </template>
