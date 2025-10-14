acc5e0b54e32fe6a6e719beabbc6bb162ee7daee
Bebe Jane <bjornthief@gmail.com>
2025-10-14 17:28:38 +0200

    update

--- modules/datocms-cma-schema-builder/index.ts
+++ modules/datocms-cma-schema-builder/index.ts
@@ -3,7 +3,6 @@ import type { Types } from '@graphql-codegen/plugin-helpers';
 import type { Nuxt } from 'nuxt/schema';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
-import { run } from '@datocms/cli';
 import { execSync } from 'node:child_process';
 
 type CodegenModuleOptions = Types.Config;
@@ -20,17 +19,11 @@ export default defineNuxtModule<CodegenModuleOptions>({
 	},
 	async setup(options: CodegenModuleOptions, nuxt: Nuxt) {
 		async function generateCode() {
-			const res = execSync(`pnpx datocms schema:generate ${appDir}/types/datocms-cma-schema.ts`);
+			const res = execSync(`pnpx @datocms/cli schema:generate ${appDir}/types/datocms-cma-schema.ts`);
 			console.log(res);
 		}
 
 		nuxt.hook('build:before', generateCode);
 		nuxt.hook('builder:watch', generateCode);
-	},
-	moduleDependencies: {
-		'@datocms/cli': {
-			version: '>=3.1.4',
-			optional: true,
-		},
 	},
 });
