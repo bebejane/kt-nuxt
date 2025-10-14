954d1b6972d36777bd895920657639d31783fcc1
Bebe Jane <bjornthief@gmail.com>
2025-10-14 20:23:18 +0200

    update

---
+++ app/components/Markdown.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import VueMarkdown from 'vue-markdown-render';
+
+const props = defineProps({
+	source: String,
+});
+</script>
+
+<template :if="props.source !== undefined">
+	<VueMarkdown :source="props.source as string" />
+</template>
