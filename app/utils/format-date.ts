71a6cdbae76643144356e4b3e010039544108163
Bebe Jane <bjornthief@gmail.com>
2025-10-14 15:04:35 +0200

    up

--- app/utils/format-date.ts
+++ app/utils/format-date.ts
@@ -1,8 +1,8 @@
-export default function formatDate(date: string, locale = 'en-US') {
+export default function (date: string, locale = 'en-US', month: Intl.DateTimeFormatOptions['month'] = 'long') {
 	const dateObj = new Date(date);
 	const options: Intl.DateTimeFormatOptions = {
 		year: 'numeric',
-		month: 'long',
+		month,
 		day: 'numeric',
 	};
 	return dateObj.toLocaleDateString(locale, options);
